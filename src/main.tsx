c5909e8899364e8868c334e57df68840725eaaf6
Maxwell Gerber <max@stytch.com>
2025-03-31 17:10:41 -0700

    chore: Add in-app misconfiguration detection

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Setup from "./Setup.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Setup>
+            <App/>
+        </Setup>
+    </StrictMode>,
 )
